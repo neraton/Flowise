9e680f8b231a391b3f14cc7aeda0987a5e39c986
automaton82 <terrence.sheflin@gmail.com>
2024-05-07 14:20:56 -0400

    Adding GA

--- packages/ui/src/App.jsx
+++ packages/ui/src/App.jsx
@@ -1,5 +1,9 @@
 import { useSelector } from 'react-redux'
 
+// GA.
+import { useEffect } from 'react'
+import ReactGA from 'react-ga'
+
 import { ThemeProvider } from '@mui/material/styles'
 import { CssBaseline, StyledEngineProvider } from '@mui/material'
 
@@ -14,8 +18,16 @@ import NavigationScroll from '@/layout/NavigationScroll'
 
 // ==============================|| APP ||============================== //
 
+// GA.
+ReactGA.initialize('G-P3JPKVNXJY')
+
 const App = () => {
     const customization = useSelector((state) => state.customization)
+
+    // GA.
+    useEffect(() => {
+        ReactGA.pageview(window.location.pathname + window.location.search)
+    }, [])
 
     return (
         <StyledEngineProvider injectFirst>
